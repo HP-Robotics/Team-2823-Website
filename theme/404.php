19150d5ee3af7d9482545cb36d86c8f9ee94c017
Misha Prasolov <mishanet123@gmail.com>
2013-12-08 19:30:52 -0600

    Updated wordpress theme links, etc.

--- theme/404.php
+++ theme/404.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
-<div id="content">
-<div id="post-0" class="post error404 not-found">
-<h1 class="entry-title"><?php _e('Not Found', 'blankslate'); ?></h1>
-<div class="entry-content">
-<p><?php _e('Nothing found for the requested page. Try a search instead?', 'blankslate'); ?></p>
-<?php get_search_form(); ?>
-</div>
-</div>
-</div>
+    <div id="content">
+        <div id="post-0" class="post error404 not-found">
+            <h1 class="entry-title"><?php _e('Not Found', 'blankslate'); ?></h1>
+            <div class="entry-content">
+                <p><?php _e('We got nuthin.', 'blankslate'); ?></p>
+            </div>
+        </div>
+    </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
