aa623432c91bcc9d17a308ebc5e74e24bcb01d01
Misha Prasolov <mishanet123@gmail.com>
2013-12-10 11:30:16 -0600

    Adding the images, AGAIN

--- wordpress/wp-content/themes/misha-theme/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-<div id="search-inputs">
-<input type="text" value="" name="s" id="s" />
-<input type="submit" id="searchsubmit" value="Search" />
-</div>
-</form>
