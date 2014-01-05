67da8937362b691b6c17d8e3ee74b6227ce8d88e
Misha Prasolov <mishanet123@gmail.com>
2014-01-05 01:13:36 -0600

    Final fixes for tonight

--- Resources/2014 Docs/gamemanual.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file="2014 Game Manual - Revision B.pdf"
-header("Content-disposition: attachment; filename=2014 Game Manual - Revision B.pdf");
-header("Content-type: application/pdf");
-readfile("2014 Game Manual - Revision B.pdf");
-header('Content-Length: ' . filesize($file));
-?>
