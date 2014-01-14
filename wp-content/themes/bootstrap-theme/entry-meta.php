d93e05104e717fd8c0724cd5ed95079126b90dc7
Tate Bosler <tatebosler@gmail.com>
2014-01-14 15:25:03 -0600

    Made navbar appear only at the top of the page (and not slide in down)

--- wp-content/themes/bootstrap-theme/entry-meta.php
+++
@@ -1,9 +0,0 @@
-<?php global $authordata; ?>
-<div class="entry-meta">
-<span class="meta-prep meta-prep-author"><?php _e('By', 'blankslate'); ?> </span>
-<span class="author vcard"><a class="url fn n" href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" title="<?php printf( __( 'View all articles by %s', 'blankslate' ), $authordata->display_name ); ?>"><?php the_author(); ?></a></span>
-<span class="meta-sep"> | </span>
-<span class="meta-prep meta-prep-entry-date"><?php _e('Published', 'blankslate'); ?> </span>
-<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
-<?php edit_post_link( __( 'Edit', 'blankslate' ), "<span class=\"meta-sep\"> | </span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>
-</div>
--- wp-content/themes/bootstrap-theme/nav-below.php
+++
@@ -1,6 +0,0 @@
-<?php global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
-<div id="nav-below" class="navigation">
-<div class="nav-previous"><?php next_posts_link(sprintf(__( '%s older articles', 'blankslate' ),'<span class="meta-nav">&larr;</span>')) ?></div>
-<div class="nav-next"><?php previous_posts_link(sprintf(__( 'newer articles %s', 'blankslate' ),'<span class="meta-nav">&rarr;</span>')) ?></div>
-</div>
-<?php } ?>
